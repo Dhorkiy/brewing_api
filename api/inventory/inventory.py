54dd4d7b79855acc072ad0b50935838b05b0357e
Oscar Lytz <oscar.lytz@gmail.com>
2017-03-16 13:24:52 +0200

    Removed a link, added DB model to ORM
    Removed a link in the compose file and replicated our DB model into the
    SQLAlchemy ORM

--- api/inventory/inventory.py
+++ api/inventory/inventory.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bryggeriklubben:BryggeriKlubben@mysql/inventory'
+
+class Inventory(db.model):
+    __tablename__ = 'ingredients'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.VARCHAR, nullable=False)
+    description = db.Column(db.VARCHAR, nullable=True)
+    alpha = db.Column(db.FLOAT, nullable=True)
+    amount = db.Column(db.FLOAT, nullable=False)
+    date = db.Column(db.DATE)
+    type = db.Column(db.VARCHAR, nullable=False)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
