5fc72859804e1b924c7a79f017924bc9f249a4b4
Oscar Lytz <oscar.lytz@gmail.com>
2017-03-26 21:44:03 +0300

    Did some small db script changes and fixed nginx

--- db/scripts/sql_import.sh
+++ db/scripts/sql_import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mysql -u root -p'testing' inventory<<EOFMYSQL
 GRANT ALL ON inventory.* TO 'bryggeriklubben'@'%';
-LOAD DATA LOCAL INFILE 'dummy_data.csv' INTO TABLE ingredients FIELDS TERMINATED BY ','  LINES TERMINATED BY '\n'  (name, description, alpha, amount, date, type)
+LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/dummy_data.csv' INTO TABLE ingredients FIELDS TERMINATED BY ','  LINES TERMINATED BY '\n'  (name, description, alpha, amount, date, type)
 EOFMYSQL
