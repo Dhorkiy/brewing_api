a518031da5439fac948e49136b024f71f0e45316
Kasper Rahikka <kasper.rahikka@gmail.com>
2017-03-26 19:31:28 +0300

    Added more dummy data with same name, changed import script to not include id - database AI does that automatically

--- db/scripts/sql_import.sh
+++ db/scripts/sql_import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mysql -u root -p'testing' inventory<<EOFMYSQL
 GRANT ALL ON inventory.* TO 'bryggeriklubben'@'%';
-LOAD DATA LOCAL INFILE 'dummy_data.csv' INTO TABLE ingredients FIELDS TERMINATED BY ','  LINES TERMINATED BY '\n'  (id, name, description, alpha, amount, date, type)
+LOAD DATA LOCAL INFILE 'dummy_data.csv' INTO TABLE ingredients FIELDS TERMINATED BY ','  LINES TERMINATED BY '\n'  (name, description, alpha, amount, date, type)
 EOFMYSQL
